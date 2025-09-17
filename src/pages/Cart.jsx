1428b4510bb38235917135098386f5f455bd82ea
hareeshvs72 <hareeshvs72@gmail.com>
2025-09-17 10:56:00 +0530

    day 6

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,61 +1,103 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
-import { Link } from 'react-router-dom'
-<Header />
+import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { removeCartItem ,incremnetQuantity,decremnetQuantity,emptyCart} from '../redux/slice/cartSlice'
+
+
+
+{/* <Header /> */ }
 
 function Cart() {
+  const dispatch = useDispatch()
+  const navigate  = useNavigate()
+  const userCArt = useSelector(state => state.cartReducer)
+
+  const [totaCart, setTotalCart] = useState(0)
+  useEffect(() => {
+    if (userCArt?.length > 0) {
+      setTotalCart(Math.ceil(userCArt?.map((item) => item.totalprice)?.reduce((prev, curr) => prev + curr)))
+    }
+  }, [userCArt])
+
+  const handileDecremnetQuantity = (item)=>{
+        if(item.quantity>1){
+             dispatch(decremnetQuantity(item?.id))
+        }
+        else{
+          dispatch(removeCartItem(item?.id))
+
+        }
+  }
+
+  const checkOut = ()=>{
+    dispatch(emptyCart())
+    alert("Your Order Has Been Placed Successfully... Thank You For Shopping With Us !!!!")
+    navigate('/')
+  }
+
   return (
     <>
       <Header />
       <div className='pt-17 pb-5 mx-5  '>
         <h1 className='text-3xl font-bold text-center text-red-500 my-10'> Cart Summary</h1>
-        <div className="grid grid-cols-3 gap-4 my-5">
-          <div className="col-span-2 rounded shadow p-5">
-            <table className='table-auto p-2 w-full' >
-              <thead>
-                <tr>
-                  <td className='font-semibold'>#</td>
-                  <td className='font-semibold'>Name</td>
-                  <td className='font-semibold'>Image</td>
-                  <td className='font-semibold'>Quantity</td>
-                  <td className='font-semibold'>Price</td>
-                  <td className='font-semibold'>...</td>
+        {
+          userCArt?.length > 0 ?
+            <div className="grid grid-cols-3 gap-4 my-5">
+              <div className="col-span-2 rounded shadow p-5">
+                <table className='table-auto p-2 w-full' >
+                  <thead>
+                    <tr>
+                      <td className='font-semibold'>#</td>
+                      <td className='font-semibold'>Name</td>
+                      <td className='font-semibold'>Image</td>
+                      <td className='font-semibold'>Quantity</td>
+                      <td className='font-semibold'>Price</td>
+                      <td className='font-semibold'>...</td>
 
 
-                </tr>
-              </thead>
-              <tbody>
-                {/* duplicate according to cart array */}
-                <tr>
-                  <td>1</td>
-                  <td>Title</td>
-                  <td><img width={'70px'} height={'70px'} src="https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp" alt="produt" /></td>
-                  <td>
-                    <div className="flex">
-                      <button className="font-bold"> - </button>
-                      <input type="text" value={10} className='rounded p-1 mx-1'  style={{width:'40px'}} readOnly/>
-                      <button className="font-bold"> + </button>
-                    </div>
-                  </td>
-                  <td>$300</td>
-                  <td><button><i className="fa-solid fa-trash text-red-500"></i></button></td>
-                </tr>
-              </tbody>
-             
-            </table>
-             <div className='float-right'>
-                  <button className=' bg-red-600 p-2 rounded mt-4 font-bold text-white mx-2'>Empty CArt</button>
-                  <Link  className=' bg-blue-600 p-2 rounded mt-4 font-bold text-white mx-2 inline-block'>Shop More</Link>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {/* duplicate according to cart array */}
+                    {
+                      userCArt?.map((item, index) => (
+                        <tr key={index}>
+                          <td>{index + 1}</td>
+                          <td><Link to={`/${item?.id}/view`} className='text-blue-500 underline' >{item?.title.slice(0, 20)}...</Link></td>
+                          <td><img width={'70px'} height={'70px'} src={item?.thumbnail} alt="produt" /></td>
+                          <td>
+                            <div className="flex">
+                              <button className="font-bold" onClick={()=>handileDecremnetQuantity(item)} > - </button>
+                              <input type="text" value={item?.quantity} className='rounded p-1 mx-1' style={{ width: '40px' }} readOnly />
+                              <button className="font-bold" onClick={()=>dispatch(incremnetQuantity(item?.id))} > + </button>
+                            </div>
+                          </td>
+                          <td>${item?.totalprice}</td>
+                          <td><button onClick={()=>dispatch(removeCartItem(item?.id))} ><i className="fa-solid fa-trash text-red-500"></i></button></td>
+                        </tr>
+                      ))
+
+                    }
+                  </tbody>
+
+                </table>
+                <div className='float-right'>
+                  <button className=' bg-red-600 p-2 rounded mt-4 font-bold text-white mx-2' onClick={()=>dispatch(emptyCart())} >Empty CArt</button>
+                  <Link to={'/'} className=' bg-blue-600 p-2 rounded mt-4 font-bold text-white mx-2 inline-block'>Shop More</Link>
+                </div>
+
               </div>
-            
-          </div>
-          <div className="col-span-1 rounded shadow p-5">
-              <h1 className='font-bold text-2xl'>Total Amount : <span className='text-red-500'>$ 500</span></h1>
-              <div className='w-full h-1 rounded  bg-black  mb-5 mt-2 '></div>
-              <button className='w-full bg-green-500 p-1 rounded mt-5 font-bold text-white'>CHECK OUT</button>
-            </div>
+              <div className="col-span-1 rounded shadow p-5">
+                <h1 className='font-bold text-2xl'>Total Amount : <span className='text-red-500'>$ {totaCart}</span></h1>
+                <div className='w-full h-1 rounded  bg-black  mb-5 mt-2 '></div>
+                <button className='w-full bg-green-500 p-1 rounded mt-5 font-bold text-white'onClick={checkOut} >CHECK OUT</button>
+              </div>
 
-        </div>
+            </div>
+            :
+            <p className='font-bold my-10 text-xl' >Your Cart IS Empty...</p>
+        }
 
       </div>
 
--- src/redux/slice/cartSlice.js
+++ src/redux/slice/cartSlice.js
@@ -20,10 +20,34 @@ const cartSlice = createSlice({
             //   add item to cart 
             state.push({...action.payload,quantity:1,totalprice:action.payload.price})
             }
+        },
+        // remove item from cart 
+        removeCartItem:(state,action)=>{
+           return state.filter(item=>item.id != action.payload)
+        },
+        // incremnet quantity
+        incremnetQuantity:(state,action)=>{
+              // find product in the state 
+            const existingProduct = state.find(item=>item.id == action.payload)
+            existingProduct.quantity++
+            existingProduct.totalprice = existingProduct.quantity * existingProduct.price
+              const remingProduct  = state?.filter(item=>item.id != action.payload)
+                state = [...remingProduct,existingProduct]
+        },
+          decremnetQuantity:(state,action)=>{
+              // find product in the state 
+            const existingProduct = state.find(item=>item.id == action.payload)
+            existingProduct.quantity--
+            existingProduct.totalprice = existingProduct.quantity * existingProduct.price
+              const remingProduct  = state?.filter(item=>item.id != action.payload)
+                state = [...remingProduct,existingProduct]
+        },
+        emptyCart:(state,action)=>{
+          return state = []
         }
     }
 
 })
 
-export const  {addToCart} = cartSlice.actions
+export const  {addToCart,removeCartItem,incremnetQuantity,decremnetQuantity,emptyCart} = cartSlice.actions
 export default cartSlice.reducer
